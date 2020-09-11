febebb7f5d5cd1e8b49a57a3ba7ec7366581ef35
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-11 12:49:02 -0400

    Add PoemFacade spec

---
+++ spec/facades/poem_facade_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Poem Facade' do
+  before(:each) do
+    @poem_facade = PoemFacade.new('Emily')
+  end
+
+  it "#get_poems_data" do
+    poems = @poems_facade.get_poems_data
+    expect(poems).to be_a(Array)
+  end
+end
